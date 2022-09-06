b5518875396bd067dac345959155c9904442daff
Rok Gregorič <rok.gregoric@gmail.com>
2022-09-06 10:10:19 +0200

    Refactor.

--- ThreadSafeDictionary.swift
+++ ThreadSafeDictionary.swift
@@ -4,78 +4,25 @@
 //  Created by Shashank on 29/10/20.
 //
 
+import Foundation
+
 class ThreadSafeDictionary<V: Hashable,T>: Collection {
+  private var dictionary: [V: T]
+  private let concurrentQueue = DispatchQueue(label: "Dictionary Barrier Queue", attributes: .concurrent)
 
-    private var dictionary: [V: T]
-    private let concurrentQueue = DispatchQueue(label: "Dictionary Barrier Queue",
-                                                attributes: .concurrent)
+  var keys: Dictionary<V, T>.Keys { concurrentQueue.sync { dictionary.keys } }
+  var values: Dictionary<V, T>.Values { concurrentQueue.sync { dictionary.values } }
+  var startIndex: Dictionary<V, T>.Index { concurrentQueue.sync { dictionary.startIndex } }
+  var endIndex: Dictionary<V, T>.Index { concurrentQueue.sync { dictionary.endIndex } }
 
-    var keys: Dictionary<V, T>.Keys {
-        self.concurrentQueue.sync {
-            return self.dictionary.keys
-        }
-    }
+  init(dict: [V: T] = [V:T]()) { dictionary = dict }
 
-    var values: Dictionary<V, T>.Values {
-        self.concurrentQueue.sync {
-            return self.dictionary.values
-        }
-    }
-
-    var startIndex: Dictionary<V, T>.Index {
-        self.concurrentQueue.sync {
-            return self.dictionary.startIndex
-        }
-    }
-
-    var endIndex: Dictionary<V, T>.Index {
-        self.concurrentQueue.sync {
-            return self.dictionary.endIndex
-        }
-    }
-
-    init(dict: [V: T] = [V:T]()) {
-        self.dictionary = dict
-    }
-    // this is because it is an apple protocol method
-    // swiftlint:disable identifier_name
-    func index(after i: Dictionary<V, T>.Index) -> Dictionary<V, T>.Index {
-        self.concurrentQueue.sync {
-            return self.dictionary.index(after: i)
-        }
-    }
-    // swiftlint:enable identifier_name
-
-    subscript(key: V) -> T? {
-        set(newValue) {
-            self.concurrentQueue.async(flags: .barrier) {[weak self] in
-                self?.dictionary[key] = newValue
-            }
-        }
-        get {
-            self.concurrentQueue.sync {
-                return self.dictionary[key]
-            }
-        }
-    }
-
-    // has implicity get
-    subscript(index: Dictionary<V, T>.Index) -> Dictionary<V, T>.Element {
-        self.concurrentQueue.sync {
-            return self.dictionary[index]
-        }
-    }
-    
-    func removeValue(forKey key: V) {
-        self.concurrentQueue.async(flags: .barrier) {[weak self] in
-            self?.dictionary.removeValue(forKey: key)
-        }
-    }
-
-    func removeAll() {
-        self.concurrentQueue.async(flags: .barrier) {[weak self] in
-            self?.dictionary.removeAll()
-        }
-    }
-
+  func index(after i: Dictionary<V, T>.Index) -> Dictionary<V, T>.Index { concurrentQueue.sync { dictionary.index(after: i) } }
+  subscript(key: V) -> T? {
+    set { concurrentQueue.async(flags: .barrier) { [weak self] in self?.dictionary[key] = newValue } }
+    get { concurrentQueue.sync { dictionary[key] } }
+  }
+  subscript(index: Dictionary<V, T>.Index) -> Dictionary<V, T>.Element { concurrentQueue.sync { dictionary[index] } }
+  func removeValue(forKey key: V) { concurrentQueue.async(flags: .barrier) { [weak self] in self?.dictionary.removeValue(forKey: key) } }
+  func removeAll() { concurrentQueue.async(flags: .barrier) { [weak self] in self?.dictionary.removeAll() } }
 }
